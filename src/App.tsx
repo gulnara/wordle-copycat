1e0cba41ec78bdb3b0e505c288a83acabed08c48
Gulnara <gmirzaka@gmail.com>
2024-03-06 12:01:10 -0500

    Added guess functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,47 @@ const MAX_ATTEMPTS = 6;
 function App() {
 	const [randomWord, setRandomWord] = useState("");
 	const [guesses, setGuesses] = useState(Array(MAX_ATTEMPTS).fill(null));
+	const [currentGuess, setCurrentGuess] = useState("");
+	const [isGameOver, setIsGameOver] = useState(false);
+
+	useEffect(() => {
+		const handleKeyDown = (event: any) => {
+			if (isGameOver) {
+				return;
+			}
+
+			if (event.key === "Backspace") {
+				setCurrentGuess(currentGuess.slice(0, -1));
+				return;
+			}
+
+			if (event.key === "Enter") {
+				if (currentGuess.length !== 5) {
+					return;
+				}
+
+				const newGuesses = [...guesses];
+				newGuesses[guesses.findIndex((val) => val == null)] = currentGuess;
+				setGuesses(newGuesses);
+				setCurrentGuess("");
+
+				const isCorrect = randomWord === currentGuess;
+				if (isCorrect) {
+					setIsGameOver(true);
+				}
+			}
+
+			if (currentGuess.length >= 5) {
+				return;
+			}
+
+			setCurrentGuess((oldGuess) => oldGuess + event.key);
+		};
+
+		window.addEventListener("keydown", handleKeyDown);
+
+		return () => window.removeEventListener("keydown", handleKeyDown);
+	}, [currentGuess, guesses, randomWord, isGameOver]);
 
 	useEffect(() => {
 		const getWords = async () => {
@@ -25,8 +66,15 @@ function App() {
 		<div className="App">
 			<h1>Wordl</h1>
 			<div className="board">
-				{guesses.map((guess: string) => {
-					return <Line guess={guess || ""} />;
+				{guesses.map((guess, i) => {
+					const isCurrentGuess = i === guesses.findIndex((val) => val == null);
+					return (
+						<Line
+							guess={isCurrentGuess ? currentGuess : guess ?? ""}
+							solution={randomWord}
+							isFinal={!isCurrentGuess && guess != null}
+						/>
+					);
 				})}
 			</div>
 		</div>
--- src/Line.tsx
+++ src/Line.tsx
@@ -1,15 +1,35 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 
 const MAX_WORD_LENGTH = 5;
 
-function Line({ guess }: { guess: string }) {
+function Line({
+	guess,
+	solution,
+	isFinal,
+}: {
+	guess: string;
+	solution: string;
+	isFinal: boolean;
+}) {
 	const tiles = [];
 
 	for (let i = 0; i < MAX_WORD_LENGTH; i++) {
 		const char = guess[i];
+		let className = "tile";
+
+		if (isFinal) {
+			if (char === solution[i]) {
+				className += " correct";
+			} else if (solution.includes(char)) {
+				className += " wrongPlace";
+			} else {
+				className += " incorrect";
+			}
+		}
+
 		tiles.push(
-			<div className="tile" key={i}>
+			<div className={className} key={i}>
 				{char}
 			</div>
 		);
