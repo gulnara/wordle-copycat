779f95d0ef801c290f1d1700fb44c8a80fb71fa3
Gulnara <gmirzaka@gmail.com>
2024-03-07 11:39:51 -0500

    Adding fun styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Line from "./Line";
 import "./App.css";
+import useWindowSize from "react-use/lib/useWindowSize";
+import Confetti from "react-confetti";
 
 const WORDL_URL = "https://random-word-api.vercel.app/api?words=10&length=5";
 const MAX_ATTEMPTS = 6;
@@ -10,6 +12,8 @@ function App() {
 	const [guesses, setGuesses] = useState(Array(MAX_ATTEMPTS).fill(null));
 	const [currentGuess, setCurrentGuess] = useState("");
 	const [isGameOver, setIsGameOver] = useState(false);
+	const { width, height } = useWindowSize();
+	const [isCorrect, setIsCorrect] = useState(false);
 
 	useEffect(() => {
 		const handleKeyDown = (event: any) => {
@@ -30,12 +34,16 @@ function App() {
 				const newGuesses = [...guesses];
 				newGuesses[guesses.findIndex((val) => val == null)] = currentGuess;
 				setGuesses(newGuesses);
-				setCurrentGuess("");
 
 				const isCorrect = randomWord === currentGuess;
 				if (isCorrect) {
+					setIsCorrect(true);
 					setIsGameOver(true);
+				} else if (guesses[4] != null) {
+					setIsGameOver(true);
+					return;
 				}
+				setCurrentGuess("");
 			}
 
 			if (currentGuess.length >= 5) {
@@ -64,18 +72,54 @@ function App() {
 
 	return (
 		<div className="App">
-			<h1>Wordl</h1>
+			<h1>Wordle The Copycat</h1>
+			{isGameOver && !isCorrect ? (
+				<>
+					<h3>Whomp, whomp, the correct word was: {randomWord}</h3>
+				</>
+			) : (
+				<></>
+			)}
+			{isCorrect ? (
+				<>
+					<Confetti width={width} height={height} />
+				</>
+			) : (
+				<></>
+			)}
 			<div className="board">
 				{guesses.map((guess, i) => {
 					const isCurrentGuess = i === guesses.findIndex((val) => val == null);
 					return (
 						<Line
+							key={i}
 							guess={isCurrentGuess ? currentGuess : guess ?? ""}
 							solution={randomWord}
 							isFinal={!isCurrentGuess && guess != null}
 						/>
 					);
 				})}
+			</div>
+
+			<div className="disclaimer">
+				<i>
+					Note: This is a personal project, built for fun by{" "}
+					<a
+						href="https://www.linkedin.com/in/gulnaram/"
+						target="_blank"
+						rel="noreferrer"
+					>
+						@gulnara
+					</a>
+					. The original Wordle game can be found on{" "}
+					<a
+						href="https://www.nytimes.com/games/wordle/index.html"
+						target="_blank"
+						rel="noreferrer"
+					>
+						nytimes page.{" "}
+					</a>
+				</i>
 			</div>
 		</div>
 	);
